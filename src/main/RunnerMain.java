8256cacb9aa9674bbae07d8fc2439ff3da829fea
Jawad Aziz Khan <jawad.aziz.khan.jak@gmail.com>
2021-11-15 19:27:19 -0700

    eclipse scaffold, separated packages and methods

---
+++ src/main/RunnerMain.java
@@ -0,0 +1,19 @@
+package main;
+
+public class RunnerMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		RunSwing();
+		RunAgency();
+	}
+
+	public static void RunSwing() {
+		System.out.println("Swing UI has been launched");
+	}
+
+	public static void RunAgency() {
+		System.out.println("The Multi Agency has been launched");
+	}
+
+}
---
+++ src/swingui/SwingMain.java
@@ -0,0 +1,5 @@
+package swingui;
+
+public class SwingMain {
+
+}
