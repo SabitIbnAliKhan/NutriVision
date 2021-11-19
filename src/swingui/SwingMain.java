4fd2d8b99e0f13fafdfd94975d1df37871433f05
SabitIbnAliKhan <sabitrahat2@gmail.com>
2021-11-18 17:47:02 -0700

    Added ActionListener and actionPerformed to the Interface

--- src/swingui/SwingMain.java
+++ src/swingui/SwingMain.java
@@ -1,26 +1,50 @@
 package swingui;
 
-import javax.swing.*;
-import java.awt.GridLayout;
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SwingMain {
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SwingMain implements ActionListener {
+
+	int count = 0;
+	private JFrame frame;
+	private JButton button;
+	private JPanel panel;
+	private JLabel label;
 
 	public SwingMain() {
-		JFrame frame = new JFrame();
+		frame = new JFrame();
 
-		JButton button = new JButton("Click Me");
+		button = new JButton("Click Me");
+		button.addActionListener(this);
 
-		JPanel panel = new JPanel();
-		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+		panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
 		panel.setLayout(new GridLayout(0, 1));
 		panel.add(button);
+
+		label = new JLabel("number of clicks: 0");
+		panel.add(label);
 
 		frame.add(panel, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Swing GUI");
 		frame.pack();
 		frame.setVisible(true);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		count++;
+		label.setText("number of clicks: " + count);
 
 	}
 
