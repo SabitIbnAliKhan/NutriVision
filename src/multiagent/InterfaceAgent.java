27c1ebf7c96c63c79ce8e30c76f780b721aab594
SabitIbnAliKhan <sabitrahat2@gmail.com>
2021-11-22 17:20:41 -0700

    Merged SwingMain and InterfaceAgent

--- src/multiagent/InterfaceAgent.java
+++ src/multiagent/InterfaceAgent.java
@@ -1,8 +1,18 @@
 package multiagent;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import jade.core.AID;
 import jade.core.behaviours.SimpleBehaviour;
@@ -11,6 +21,7 @@ import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import swingui.SwingMain;
 import utils.Constants;
+//package swingui;
 
 public class InterfaceAgent extends ServiceAgent {
 
@@ -105,6 +116,45 @@ public class InterfaceAgent extends ServiceAgent {
 		@Override
 		public boolean done() {
 			return finished;
+		}
+
+		public class SwingMain implements ActionListener {
+
+			int count = 0;
+			private JFrame frame;
+			private JButton button;
+			private JPanel panel;
+			private JLabel label;
+
+			public SwingMain() {
+				frame = new JFrame();
+
+				button = new JButton("Click Me");
+				button.addActionListener(this);
+
+				panel = new JPanel();
+				panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+				panel.setLayout(new GridLayout(0, 1));
+				panel.add(button);
+
+				label = new JLabel("number of clicks: 0");
+				panel.add(label);
+
+				frame.add(panel, BorderLayout.CENTER);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setTitle("Swing GUI");
+				frame.pack();
+				frame.setVisible(true);
+
+			}
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				count++;
+				label.setText("number of clicks: " + count);
+
+			}
+
 		}
 	}
 
